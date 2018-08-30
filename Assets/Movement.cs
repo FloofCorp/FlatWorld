91e2cd06c0f9a6f503d75b584cb7b8297997506d
Nero119 <theo.crawford1992@gmail.com>
2018-08-30 23:59:52 +1000

    Thank you for not giving me Intellisense, Unity2018 Fixed motion bug

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -43,7 +43,7 @@ public class Movement : MonoBehaviour {
     void ThreeDMove()
     {
         _moveVec = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if(_moveVec.magnitute > 1)
+        if(_moveVec.magnitude > 1)
         {
             _moveVec.Normalize();
         }
