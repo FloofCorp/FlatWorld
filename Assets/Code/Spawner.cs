6f403ad82c7f7f7e7d7b68d2003c6fe24b369cf6
RavenBlackhart <raven@blackhart.info>
2018-10-11 01:04:11 +0200

    Tetris is and playable! still trying to make it do fancy stuff

---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    //Groups 
+    public GameObject[] groups;
+
+	// Use this for initialization
+	void Start () {
+
+        //Spawn Initial Group
+        spawnNext(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnNext() {
+        
+        //Random Index 
+        int i = Random.Range(0, groups.Length);
+
+        //Spawn Group at Current Position 
+        Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+
+}
