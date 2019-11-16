0aaa2879b790a9e3e91068324b53ccceed8bde58
nero119 <theojcrawford@gmail.com>
2019-11-16 12:42:21 +1000

    Modified some of the movement code
    Modified movement, pulled the moveSpeed back, intergrated framerate back into the movement calculation

--- Assets/Code/PMMotor.cs
+++ Assets/Code/PMMotor.cs
@@ -11,20 +11,38 @@ public class PMMotor : MonoBehaviour {
     [SerializeField] private float _jumpSpeed =6f;
     [SerializeField] private float _gravity = 9.81f;
 
+    private bool _isfalling = false;
+
+    private static CharacterController _god;
     private static PMController _charCon;
     private float verticalVel;
 
     private void Awake()
     {
         _charCon = GetComponent<PMController>();
+        _god = GetComponent<CharacterController>();
     }
     public void UpdateMotor()
     {
         ProccessMotion();
+        GiveThrotle();
     }
 
     private void ProccessMotion()
     {
         moveVector = new Vector3(moveVector.x, verticalVel, moveVector.z);
+    }
+    private void GiveThrotle()
+    {
+        moveVector.x *= _walkSpeed;
+        moveVector *= Time.deltaTime;
+    }
+    private void GravityDrive()
+    {
+        //If the i'm grounded, begin generating gravity
+        if (!_god.isGrounded)
+        {
+            verticalVel = Mathf.Lerp(verticalVel, _gravity, Time.deltaTime);
+        }
     }
 }
